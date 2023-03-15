b7a3c8e2cd39cdfa0641546e1d365a3adaaf12ab
Dexter <dexterindonesia36@gmail.com>
2023-03-15 10:52:12 +0700

    Set textTheme and app color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.archivoTextTheme(),
+      ),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
